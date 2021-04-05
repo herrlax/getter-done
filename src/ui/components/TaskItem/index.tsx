f19a2ab00735cb8f9c0defeb396f4c2d2502b792
Mikael Malmqvist <mikael@malmqvist.it>
2021-04-05 15:30:38 +0200

    Add task component

---
+++ src/ui/components/TaskItem/index.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Task } from '../../context/tasks';
+import { styled } from 'goober';
+import Checkbox from '../Checkbox';
+
+type Props = {
+  task: Task;
+  onChange: (task: Task) => void;
+};
+
+const Label = styled('label')({
+  display: 'flex',
+  alignItems: 'center',
+  '& input': {
+    marginTop: '6px'
+  }
+});
+
+const TaskItem: React.FC<Props> = ({ task, onChange }) => {
+  const [checked, setChecked] = useState(task.done);
+
+  useEffect(() => {
+    // debounce this
+    onChange(task);
+  }, [checked]);
+
+  return (
+    <li key={task.id}>
+      <Label>
+        <Checkbox checked={checked} onChange={setChecked} />
+        {task.title}
+      </Label>
+    </li>
+  );
+};
+
+export default TaskItem;
--- src/ui/components/TaskList/index.tsx
+++ src/ui/components/TaskList/index.tsx
@@ -1,17 +1,23 @@
 import { useTasksState } from '@/ui/context/tasks';
+import { styled } from 'goober';
 import React from 'react';
+import TaskItem from '../TaskItem';
+
+const List = styled('ul')({ listStyle: 'none', margin: 0, padding: 0 });
 
 const TaskList: React.FC = () => {
   const { data } = useTasksState();
 
+  const handleChange = (task: Task) => {
+    // todo edit task
+  };
+
   return (
-    <ul>
+    <List>
       {data.map((task) => (
-        <li key={task.id}>
-          <h3>{task.title}</h3>
-        </li>
+        <TaskItem task={task} onChange={handleChange} />
       ))}
-    </ul>
+    </List>
   );
 };
 
