22392101ef178023f7da7e2e9f9a38282cc4c84f
Mikael Malmqvist <mikael@malmqvist.it>
2021-04-11 14:47:18 +0200

    Add checkbox test

---
+++ src/ui/components/Checkbox/index.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import Checkbox from '.';
+
+describe('<Checkbox />', function () {
+  it('renders without crashing', () => {
+    render(<Checkbox onChange={() => {}} checked name="" />);
+  });
+
+  it('runs onChange when clicking Checkbox', async () => {
+    const spy = jest.fn();
+    const { container } = render(<Checkbox onChange={spy} name="check" />);
+
+    const checkInput = container.querySelector('input[name="check"]');
+
+    if (checkInput) {
+      fireEvent.click(checkInput);
+    }
+
+    await waitFor(() => expect(spy).toHaveBeenCalledTimes(1));
+  });
+});
