04a8a35ddd768f460c377178908a7a210c28234e
Mikael Malmqvist <mikael@malmqvist.it>
2022-05-23 11:15:28 +0200

    Update font loading in webpack

--- webpack.react.js
+++ webpack.react.js
@@ -30,13 +30,16 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/
+      },
+      {
+        test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
+              outputPath: 'assets/icons/'
             }
           }
         ]
